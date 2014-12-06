63698921bd7a743f482e9e2d1c909fded61dc6a4
Andrew Waterman <waterman@cs.berkeley.edu>
2014-12-05 20:37:21 -0800

    Update blast to dump results at the end

--- support/blast.cc
+++ support/blast.cc
@@ -10,21 +10,34 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+struct sample {
+  uint64_t tag;
+  double recv_start;
+  double recv_stop;
+  double send_start;
+  double send_stop;
+};
+
 static int nc;
 static int rpc;
 static const char* url;
 static struct sockaddr_in remote;
-static std::atomic<uint64_t> num_responses = ATOMIC_VAR_INIT(0);
-static struct timeval t0, t1;
+static std::atomic<uint64_t> num_tags = ATOMIC_VAR_INIT(0);
+static double t0, t1;
+static uint64_t max_samples;
 
-static void print_stats()
+static double gettime()
 {
   struct timeval t;
   gettimeofday(&t, 0);
-  double tdiff0 = (t.tv_sec - t0.tv_sec) + 1e-6*(t.tv_usec - t0.tv_usec);
-  double tdiff1 = (t.tv_sec - t1.tv_sec) + 1e-6*(t.tv_usec - t1.tv_usec);
-  if (tdiff1 > 5) {
-    printf("%d requests/sec\n", (int)(num_responses / tdiff0));
+  return t.tv_sec + 1e-6*t.tv_usec;
+}
+
+static void print_stats()
+{
+  double t = gettime();
+  if (t - t1 >= 5) {
+    printf("%d requests/sec\n", (int)(num_tags / (t - t0)));
     t1 = t;
   }
 }
@@ -74,25 +87,38 @@ static void* connection(void* arg)
 
   std::string query = std::string("GET ") + url + " HTTP/1.1\r\n\r\n";
 
-  for (int i = 0; i < rpc; i++)
+  std::vector<sample>& samples = *(new std::vector<sample>(rpc));
+  for (int i = 0; i < rpc; i++) {
+    samples[i].tag = std::atomic_fetch_add(&num_tags, uint64_t(1));
+    samples[i].send_start = gettime();
     send_query(sock, query);
+    samples[i].send_stop = gettime();
+  }
 
-  while (1) {
+  for (uint64_t i = rpc; ; i++) {
+    samples[i - rpc].recv_start = gettime();
     std::vector<char> buf = receive_response(sock);
-    send_query(sock, query);
-    std::atomic_fetch_add(&num_responses, uint64_t(1));
+    samples[i - rpc].recv_stop = gettime();
+    if (max_samples && samples[i - rpc].tag >= max_samples)
+      break;
 
-    if (id == 0)
+    samples.resize(i + 1);
+    samples[i].tag = std::atomic_fetch_add(&num_tags, uint64_t(1));
+    samples[i].send_start = samples[i - rpc].recv_stop;
+    send_query(sock, query);
+    samples[i].send_stop = gettime();
+
+    if (!max_samples && id == 0)
       print_stats();
   }
 
-  return NULL;
+  return &samples;
 }
 
 int main(int argc, char** argv)
 {
-  if (argc != 6) {
-    printf("usage: blast <ip> <port> <url> <connections> <reqs per conn>\n");
+  if (argc < 6 || argc > 7) {
+    printf("usage: blast <ip> <port> <url> <connections> <reqs per conn> [total reqs]\n");
     return 1;
   }
 
@@ -106,15 +132,29 @@ int main(int argc, char** argv)
   url = argv[3];
   nc = atoi(argv[4]);
   rpc = atoi(argv[5]);
-  gettimeofday(&t0, 0);
-  t1 = t0;
+  if (argc >= 7)
+    max_samples = atoll(argv[6]);
 
-  for (int i = 1; i < nc; i++) {
-    pthread_t pt;
-    if (pthread_create(&pt, 0, connection, (void*)(uintptr_t)i) < 0) {
+  t0 = t1 = gettime();
+
+  std::vector<pthread_t> threads(nc);
+  for (int i = 0; i < nc; i++) {
+    if (pthread_create(&threads[i], 0, connection, (void*)(uintptr_t)i) < 0) {
       perror("can't make threads");
       return 1;
     }
-  } 
-  connection(0);
+  }
+
+  for (int i = 0; i < nc; i++) {
+    std::vector<sample>* samples;
+    pthread_join(threads[i], (void**)&samples);
+    for (sample& x : *samples) if (x.tag < max_samples) {
+      printf("EV_CALL_SEND_START:%lld:%f\n", (long long)x.tag, x.send_start);
+      printf("EV_CALL_SEND_STOP:%lld:%f\n", (long long)x.tag, x.send_stop);
+      printf("EV_CALL_RECV_START:%lld:%f\n", (long long)x.tag, x.recv_start);
+      printf("EV_CALL_DESTROYED:%lld:%f\n", (long long)x.tag, x.recv_stop);
+    }
+  }
+
+  return 0;
 }
